57fb71e6ee72d52517573373d1926b448c9f277b
Ana Lucia <Usuario@LAPTOP-ID4HLSDH>
2026-08-01 17:26:00 -0600

    Agrega gestión de materiales en la base de datos con metodos registrar, listar, buscar por codigo y actualizar material

--- src/datos/MaterialDAO.java
+++ src/datos/MaterialDAO.java
@@ -4,14 +4,18 @@
  */
 package datos;
 
+import modelo.EstadoMaterial;
 import modelo.Libro;
 import modelo.MaterialBibliografico;
 import modelo.Revista;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MaterialDAO {
     
@@ -66,4 +70,159 @@ public class MaterialDAO {
         }
     }
     
+    // Metodo para listar materiales
+    public List<MaterialBibliografico> listar() {
+    
+        List<MaterialBibliografico> listaMateriales = new ArrayList<>();
+        
+        String sql = "SELECT * FROM materiales";
+        
+        try (
+                Connection con = ConexionBD.conectar();
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+            ) {
+            
+            while (rs.next()) {
+                
+                int id = rs.getInt("id_material");
+                String codigo = rs.getString("codigo");
+                String titulo = rs.getString("titulo");
+                String autor = rs.getString("autor");
+                int anio = rs.getInt("anio_publicacion");
+                String tipoMaterial = rs.getString("tipo_material");
+                
+                EstadoMaterial estado = EstadoMaterial.valueOf(
+                        rs.getString("estado")
+                );
+                
+                MaterialBibliografico material;
+                
+                if (tipoMaterial.equals("LIBRO")) {
+                    
+                    String isbn = rs.getString("isbn");
+                    
+                    material = new Libro (
+                            id,
+                            codigo,
+                            titulo,
+                            autor,
+                            anio,
+                            estado,
+                            isbn
+                    );
+                } else {
+                    
+                    int numeroEdicion = rs.getInt("numero_edicion");
+                    
+                    material = new Revista(
+                            id,
+                            codigo,
+                            titulo,
+                            autor,
+                            anio,
+                            estado,
+                            numeroEdicion
+                    );
+                }
+                
+                listaMateriales.add(material);
+            }           
+        } catch (SQLException e) {
+            System.out.println("Error al listar los materiales");
+            e.printStackTrace();
+        }
+        return listaMateriales;
+    }
+    
+    // Metodo para buscar por codigo
+    public MaterialBibliografico buscarPorCodigo(String codigoBuscado) {
+
+        String sql = "SELECT * FROM materiales WHERE codigo = ?";
+
+        try (
+                Connection con = ConexionBD.conectar();
+                PreparedStatement ps = con.prepareStatement(sql)
+            ) {
+
+            ps.setString(1, codigoBuscado);
+
+            try (ResultSet rs = ps.executeQuery()) {
+
+                if (rs.next()) {
+
+                    int id = rs.getInt("id_material");
+                    String codigo = rs.getString("codigo");
+                    String titulo = rs.getString("titulo");
+                    String autor = rs.getString("autor");
+                    int anio = rs.getInt("anio_publicacion");
+                    String tipoMaterial = rs.getString("tipo_material");
+
+                    EstadoMaterial estado = EstadoMaterial.valueOf(
+                            rs.getString("estado")
+                    );
+
+                    if (tipoMaterial.equals("LIBRO")) {
+
+                        String isbn = rs.getString("isbn");
+
+                        return new Libro(
+                                id,
+                                codigo,
+                                titulo,
+                                autor,
+                                anio,
+                                estado,
+                                isbn
+                        );
+
+                    } else {
+
+                        int numeroEdicion = rs.getInt("numero_edicion");
+
+                        return new Revista(
+                                id,
+                                codigo,
+                                titulo,
+                                autor,
+                                anio,
+                                estado,
+                                numeroEdicion
+                        );
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error al buscar el material");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    
+    // Metodo para actualizar el estado de un material
+    public boolean actualizarEstado( int idMaterial, EstadoMaterial nuevoEstado) {
+        
+        String sql = "UPDATE materiales "
+                + "SET estado = ? "
+                + "WHERE id_material = ?";
+        
+        try(
+                Connection con = ConexionBD.conectar();
+                PreparedStatement ps = con.prepareStatement(sql);
+            ) {
+            
+            ps.setString(1, nuevoEstado.name());
+            ps.setInt(2, idMaterial);
+            
+            int filasActualizadas = ps.executeUpdate();
+            
+            return filasActualizadas > 0;
+        } catch (SQLException e) {
+            System.out.println("Error al actualizar el estado del material");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
